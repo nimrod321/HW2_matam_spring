c5ab30a5bf54430eb457600078274c996232c745
Nimrod Allouche <nimrodal@campus.technion.ac.il>
2025-06-04 17:05:37 +0300

    add bounty setters

--- dry/Pirate.cpp
+++ dry/Pirate.cpp
@@ -4,7 +4,7 @@
 Pirate::Pirate() = default;
 
 
-Pirate::Pirate(const string& name): name(name) {}
+Pirate::Pirate(const string& name, int bounty) : name(name), bounty(bounty) {} 
 
 
 void Pirate::setName(const string& name){
@@ -16,6 +16,13 @@ std::string Pirate::getName(){
     return name;
 }
 
+void Pirate::setBounty(int bounty){
+    this->bounty = bounty;
+}
+
+int Pirate::getBounty() const {
+    return bounty;
+}
 
 std::ostream &operator<<(std::ostream &os, const Pirate &pirate){
     os << pirate.name;
--- dry/Pirate.h
+++ dry/Pirate.h
@@ -8,18 +8,23 @@ using std::string;
 class Pirate {
 private:
     string name;
+    int bounty;
 
 public:
 
     Pirate();
 
-    Pirate(const string& name);
+    Pirate(const string& name, int bounty);
 
     ~Pirate() = default;
 
     void setName(const string& name);
 
     string getName();
+
+    void setBounty(int bounty);
+
+    int getBounty() const;
 
     friend std::ostream &operator<<(std::ostream &os, const Pirate &pirate);
 };
